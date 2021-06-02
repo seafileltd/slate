c9fd9b39dc416aef89eb99018c3df00f39f00d9c
cainiao222 <1325442034@qq.com>
2021-06-02 15:51:39 +0800

    fix code

--- packages/slate-react/src/utils/environment.ts
+++ packages/slate-react/src/utils/environment.ts
@@ -44,3 +44,4 @@ export const HAS_BEFORE_INPUT_SUPPORT =
   globalThis.InputEvent &&
   // @ts-ignore The `getTargetRanges` property isn't recognized.
   typeof globalThis.InputEvent.prototype.getTargetRanges === 'function'
+  
