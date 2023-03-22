fe9b96a303433d3371a690256be38504f960e3d7
杨顺强 <978987373@qq.com>
2023-03-22 17:31:13 +0800

    add uuid code

--- packages/slate/src/transforms/general.ts
+++ packages/slate/src/transforms/general.ts
@@ -1,5 +1,5 @@
 import { createDraft, finishDraft, isDraft } from 'immer'
-import { v4 } from 'md5'
+import { nanoid } from 'nanoid'
 import {
   Ancestor,
   Descendant,
@@ -33,7 +33,7 @@ const applyToDraft = (editor: Editor, selection: Selection, op: Operation) => {
         )
       }
 
-      if (!node.id) node.id = v4()
+      if (!node.id) node.id = nanoid()
       parent.children.splice(index, 0, node)
 
       if (selection) {
@@ -301,7 +301,7 @@ const applyToDraft = (editor: Editor, selection: Selection, op: Operation) => {
         }
       }
 
-      if (!newNode.id) newNode.id = v4()
+      if (!newNode.id) newNode.id = nanoid()
       parent.children.splice(index + 1, 0, newNode)
 
       if (selection) {
